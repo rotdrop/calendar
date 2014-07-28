ab980cf23e24e5b523c9a70959d6336626ce340d
Robin Appelman <icewind@owncloud.com>
2014-07-28 15:10:09 +0200

    fix caldav when the calendar app is only enabled for specific groups

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -5,16 +5,18 @@
  * later.
  * See the COPYING-README file.
  */
-OCP\App::checkAppEnabled('calendar');
+if (!\OC::$server->getAppManager()->isInstalled('calendar')) {
+	throw new Exception('App not installed: calendar');
+}
 
-if(substr(OCP\Util::getRequestUri(),0,strlen(OC_App::getAppWebPath('calendar').'/caldav.php')) == OC_App::getAppWebPath('calendar'). '/caldav.php') {
-	$baseuri = OC_App::getAppWebPath('calendar').'/caldav.php';
+if (substr(OCP\Util::getRequestUri(), 0, strlen(OC_App::getAppWebPath('calendar') . '/caldav.php')) == OC_App::getAppWebPath('calendar') . '/caldav.php') {
+	$baseuri = OC_App::getAppWebPath('calendar') . '/caldav.php';
 }
 
 // only need authentication apps
-$RUNTIME_APPTYPES=array('authentication');
+$RUNTIME_APPTYPES = array('authentication');
 OC_App::loadApps($RUNTIME_APPTYPES);
-if(\OCP\App::isEnabled('contacts')) {
+if (\OC::$server->getAppManager()->isInstalled('contacts')) {
 	\OCP\Share::registerBackend('addressbook', 'OCA\Contacts\Share\Addressbook', 'contact');
 }
 
@@ -37,18 +39,22 @@ $calendarRoot->disableListing = true; // Disable listening
 $nodes = array(
 	$Sabre_CalDAV_Principal_Collection,
 	$calendarRoot,
-	);
+);
 
 // Fire up server
 $server = new \Sabre\DAV\Server($nodes);
 $server->httpRequest = $requestBackend;
 $server->setBaseUri($baseuri);
 // Add plugins
-$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend,'ownCloud'));
+$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend, 'ownCloud'));
 $server->addPlugin(new \Sabre\CalDAV\Plugin());
 $server->addPlugin(new \Sabre\DAVACL\Plugin());
 $server->addPlugin(new \Sabre\DAV\Browser\Plugin(false)); // Show something in the Browser, but no upload
 $server->addPlugin(new \Sabre\CalDAV\ICSExportPlugin());
+$server->addPlugin(new \OC\Connector\Sabre\AppEnabledPlugin(
+	'calendar',
+	OC::$server->getAppManager()
+));
 
 // And off we go!
 $server->exec();
